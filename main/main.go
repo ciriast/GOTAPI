7b7e63832f8a202150c95ff72e7d062bd9db064b
Carlos Irias <ciriastorres@gmail.com>
2023-10-12 05:46:54 -0600

    Make a call to the game of thrones random quotes API

--- main/main.go
+++ main/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, from my web API!")
+	url := "https://api.gameofthronesquotes.xyz/v1/random"
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(req)
 }
 
 
